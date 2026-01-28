295b174d0e716fa1b980034ef383223f8e4456c1
jpsharkey84 <jpsharkey84@yahoo.com>
2026-01-28 18:54:25 +0000

    datetime test

--- beacons/app.py
+++ beacons/app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, admin!</p>"
+
+current_datetime = datetime.datetime.now()
+print(current_datetime)
